5e54f4891a5007518a0a79826b0bd86e57d976ae
Patrick Duncan BUENAVENTURA <Patrick.BUENAVENTURA@student.tafesa.edu.au>
2025-10-25 22:50:05 +0800

    feat: Add Category CRUD functionality with user-specific access
    - Implemented `index`, `store`, `update`, and `destroy` methods in `CategoryController` for managing categories.
    - Ensured user-specific data access by filtering categories by authenticated user ID.
    - Added validation rules for category creation and updates, including `name` and `type` fields.
    - Created migration and model for `categories` table with user reference, `name`, and `type` attributes (income/expense).
    - Added foreign key constraint for user ID in the `categories` table with cascade on delete and update.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $category = Category::where('user_id', Auth::id())->get;
+
+        return response()->json([
+            'message' => "categories retrieved successfully",
+            'data' => $category
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => Auth::id(),
+            'name' => 'required|string',
+            'type' => 'required|in:income,expense'
+        ]);
+
+        $category = Category::create([
+            'name' => $request->name,
+            'type' => $request->type
+        ]);
+
+        return response()->json([
+            'message' => 'Category created successfully',
+            'data' => $category
+        ], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = Auth::id();
+        $category = Category::where('user_id', $user)->findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'sometimes|string',
+            'type' => 'sometimes|in:income,expense '
+        ]);
+
+        $category->update($validated);
+
+        return response()->json([
+            'message' => 'Category updated successfully',
+            'data' => $category
+        ], 201);
+    }
+
+    public function destroy($id)
+    {
+        $user = Auth::id();
+        $category = Category::where('user_id', $user)->findOrFail($id);
+        $category->delete();
+
+        return response()->json([
+            'message' => 'Category deleted successfully'
+        ]);
+    }
+}
