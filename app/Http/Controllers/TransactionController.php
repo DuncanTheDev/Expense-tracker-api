f9e0a183ce6294664e7825615c5c53cd9aed6cff
Patrick Duncan BUENAVENTURA <Patrick.BUENAVENTURA@student.tafesa.edu.au>
2025-10-27 23:28:11 +0800

    Create TransactionController with CRUD operations
    - Added methods to handle index, show, store, update, and destroy transactions
    - Implemented validation for transaction data on store and update
    - Ensured user authentication and authorization for transaction access
    - Used relationships to include 'category' data with transactions

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::id();
+        $transactions = Transaction::where('user_id', $user)
+            ->with('category')
+            ->get();
+
+        return response()->json([
+            'message' => 'Transactions retrieve successfully',
+            'data' => $transactions
+        ], 200);
+    }
+
+    public function show($id)
+    {
+        $user = Auth::id();
+        $transaction = Transaction::where('user_id', $user)
+            ->where('id', $id)
+            ->with('category')
+            ->firstOrFail();
+
+        return response()->json([
+            'message' => "Transaction retrieved successfully",
+            'data' => $transaction
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $user = Auth::id();
+        $request->validate([
+            'category_id' => 'required|exists:categories,id',
+            'amount' => 'required|decimal:10,2',
+            'type' => 'required|in:income,expense',
+            'description' => 'nullable|string',
+            'transaction_date' => 'required|date'
+        ]);
+
+        $transaction = Transaction::create([
+            'user_id' => $user,
+            'category_id' => $request->category_id,
+            'amount' => $request->amount,
+            'type' => $request->type,
+            'description' => $request->description,
+            'transaction_date' => $request->transaction_date
+        ]);
+
+        return response()->json([
+            'message' => 'Transaction created successfully',
+            'data' => $transaction
+        ], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = Auth::id();
+        $transaction = Transaction::where('user_id', $user)
+            ->where('id', $id)
+            ->firstOrFail($id);
+
+        $validated = $request->validate([
+            'category_id' => 'sometimes|exists:categoies,id',
+            'amount' => 'sometimes|decimal:10,2',
+            'type' => 'sometimes|in:income,expense',
+            'description' => 'nullable|string',
+            'transaction_date' => 'sometimes|date'
+        ]);
+
+        $transaction->update($validated);
+
+        return response()->json([
+            'message',
+            'Transaction updated successfully',
+            'data' => $transaction,
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $user = Auth::id();
+        $transaction = Transaction::where('user_id', $user)
+            ->where('id', $id)
+            ->firstOrFail($id);
+
+        $transaction->delete();
+
+        return response()->json([
+            'message',
+            'Transaction deleted successfully',
+        ], 200);
+    }
+}
