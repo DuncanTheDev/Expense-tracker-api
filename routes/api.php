c1de5f98b016e9202e1e5e622b04bff2b9cb58d5
Patrick Duncan BUENAVENTURA <Patrick.BUENAVENTURA@student.tafesa.edu.au>
2025-10-28 20:26:31 +0800

    Add: Define API routes for authentication, categories, and transactions
    - Set up authentication routes for user registration, login, and logout with Sanctum.
    - Created CRUD routes for categories and transactions, secured with 'auth:sanctum' middleware.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/category', [CategoryController::class, 'store']);
+    Route::put('category', [CategoryController::class, 'update']);
+    Route::delete('category', [CategoryController::class, 'destroy']);
+
+    Route::get('transactions', [TransactionController::class, 'index']);
+    Route::get('transaction/{id}', [TransactionController::class, 'show']);
+    Route::post('transaction', [TransactionController::class, 'store']);
+    Route::put('transaction', [TransactionController::class, 'update']);
+    Route::delete('transaction', [TransactionController::class, 'destroy']);
+});
